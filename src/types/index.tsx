e916fca7966f1ec6c2708b5c094a207e65ad37fa
ilkhukuk <ilkhukuk@gmail.com>
2024-01-28 23:51:54 +0300

    1

---
+++ src/types/index.tsx
@@ -0,0 +1,41 @@
+import { MouseEventHandler } from 'react';
+
+// buton bileşenin aldığı propların tipini tanımlama
+export type ButtonProps = {
+  title: string;
+  designs?: string;
+  disabled?: boolean;
+  btnType?: 'button' | 'submit';
+  handleClick?: MouseEventHandler;
+  rIcon?: string;
+};
+
+// arabanın bilgileri
+export type CarType = {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: 'fwd' | 'rwd' | 'awd' | '4wd';
+  fuel_type: 'gas' | 'diesel' | 'elecricity';
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
+};
+
+export type filterType = {
+  make?: string;
+  model?: string;
+  limit?: string;
+  fuel?: string;
+  year?: string;
+};
+
+export type OptionType = {
+  label: string;
+  value: string;
+};
+
